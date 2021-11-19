c0d6eab1635eeb30d34f06e0f63351d3bdec26e8
Talentedunicorn <dev@talentedunicorn.com>
2021-11-19 21:41:26 +0800

    Refactored scrollto top component

---
+++ components/ScrollToTop.vue
@@ -0,0 +1,56 @@
+<template>
+  <transition name="slide-down">
+      <button
+        v-show="showScrollTop"
+        v-scrollTo="scrollToId"
+        class="ScrollTop button"
+      >
+        <svg viewBox="0 0 24 24">
+          <use xlink:href="#arrow-up"></use>
+        </svg>
+      </button>
+    </transition>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    scrollToId: {
+      type: String,
+      required: true
+    }
+  },
+  data(): { showScrollTop: boolean } {
+    return {
+      showScrollTop: false,
+    }
+  },
+  mounted() {
+    // Attach scroll listener
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      this.showScrollTop = window.scrollY > window.innerHeight
+    },
+  },
+})
+</script>
+
+<style scoped>
+.ScrollTop {
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  padding: 0;
+  z-index: 3;
+}
+
+.ScrollTop svg {
+  --icon-size: 2rem;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,53 +2,6 @@
   <main id="top">
     <Icons />
     <Nuxt />
-    <transition name="slide-down">
-      <button
-        v-show="showScrollTop"
-        v-scrollTo="'top'"
-        class="ScrollTop button"
-      >
-        <svg viewBox="0 0 24 24">
-          <use xlink:href="#arrow-up"></use>
-        </svg>
-      </button>
-    </transition>
+    <ScrollToTop scroll-to-id="top" />
   </main>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  data(): { showScrollTop: boolean } {
-    return {
-      showScrollTop: false,
-    }
-  },
-  mounted() {
-    // Attach scroll listener
-    window.addEventListener('scroll', this.handleScroll)
-  },
-  beforeDestroy() {
-    window.removeEventListener('scroll', this.handleScroll)
-  },
-  methods: {
-    handleScroll() {
-      this.showScrollTop = window.scrollY > window.innerHeight
-    },
-  },
-})
-</script>
-
-<style>
-.ScrollTop {
-  position: fixed;
-  bottom: 1rem;
-  right: 1rem;
-  padding: 0;
-  z-index: 3;
-}
-
-.ScrollTop svg {
-  --icon-size: 2rem;
-}
-</style>
